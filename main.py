5b88afa7f2791d5ac953376bb93f8fccee7d0372
Poramee Promkeeree <poramee0013@gmail.com>
2023-03-04 02:07:32 +0700

    เพิ่มการRotateของBlockตัว I

--- main.py
+++ main.py
@@ -34,13 +34,49 @@ class TetrisWidget(GridLayout):
             obj = GridButton()
             self.add_widget(obj)
 
-        self.r = random.randint(1, 7)
+        self.r = 1#random.randint(1, 7)
         self.new_shape()
 
     def update_game(self, window_width, dt):
         self.fall(dt)
         self.size_change(window_width)
         self.collisions_bottom()
+
+    def rotate(self):
+        if self.shapes[-1][0].brick == "I" and self.shapes[-1][0].version == 1:
+            x = self.shapes[-1][0].position_x
+            y = self.shapes[-1][0].position_y
+            
+            if x < 1:
+                x = 1
+            elif x > 7:
+                x = 7
+            
+            self.shapes[-1][0].position_x = x - 1
+            self.shapes[-1][0].position_y = y
+            self.shapes[-1][1].position_x = x
+            self.shapes[-1][1].position_y = y
+            self.shapes[-1][2].position_x = x + 1
+            self.shapes[-1][2].position_y = y
+            self.shapes[-1][3].position_x = x + 2
+            self.shapes[-1][3].position_y = y
+            
+            self.shapes[-1][0].version = 2
+            
+        elif self.shapes[-1][0].brick == "I" and self.shapes[-1][0].version == 2:
+            x = self.shapes[-1][0].position_x
+            y = self.shapes[-1][0].position_y
+            
+            self.shapes[-1][0].position_x = x + 1
+            self.shapes[-1][0].position_y = y
+            self.shapes[-1][1].position_x = x + 1
+            self.shapes[-1][1].position_y = y + 1
+            self.shapes[-1][2].position_x = x + 1
+            self.shapes[-1][2].position_y = y + 2
+            self.shapes[-1][3].position_x = x + 1
+            self.shapes[-1][3].position_y = y + 3
+            
+            self.shapes[-1][0].version = 1
 
     def fall(self, dt):
         self.time += dt
@@ -73,50 +109,50 @@ class TetrisWidget(GridLayout):
             Color(*color)
             if self.r == 1:
 
-                shape.append(Shape(4, 20, "I", size=(0, 0)))
-                shape.append(Shape(4, 21, "I", size=(0, 0)))
-                shape.append(Shape(4, 22, "I", size=(0, 0)))
-                shape.append(Shape(4, 23, "I", size=(0, 0)))
+                shape.append(Shape(4, 20, "I", 1, size=(0, 0)))
+                shape.append(Shape(4, 21, "I", 1, size=(0, 0)))
+                shape.append(Shape(4, 22, "I", 1, size=(0, 0)))
+                shape.append(Shape(4, 23, "I", 1, size=(0, 0)))
             elif self.r == 2:
 
-                shape.append(Shape(4, 20, "O", size=(0, 0)))
-                shape.append(Shape(5, 20, "O", size=(0, 0)))
-                shape.append(Shape(4, 21, "O", size=(0, 0)))
-                shape.append(Shape(5, 21, "O", size=(0, 0)))
+                shape.append(Shape(4, 20, "O", 1, size=(0, 0)))
+                shape.append(Shape(5, 20, "O", 1, size=(0, 0)))
+                shape.append(Shape(4, 21, "O", 1, size=(0, 0)))
+                shape.append(Shape(5, 21, "O", 1, size=(0, 0)))
             elif self.r == 3:
 
-                shape.append(Shape(4, 20, "L", size=(0, 0)))
-                shape.append(Shape(4, 21, "L", size=(0, 0)))
-                shape.append(Shape(4, 22, "L", size=(0, 0)))
-                shape.append(Shape(5, 20, "L", size=(0, 0)))
+                shape.append(Shape(4, 20, "L", 1, size=(0, 0)))
+                shape.append(Shape(4, 21, "L", 1, size=(0, 0)))
+                shape.append(Shape(4, 22, "L", 1, size=(0, 0)))
+                shape.append(Shape(5, 20, "L", 1, size=(0, 0)))
             elif self.r == 4:
 
-                shape.append(Shape(4, 20, "J", size=(0, 0)))
-                shape.append(Shape(5, 20, "J", size=(0, 0)))
-                shape.append(Shape(5, 21, "J", size=(0, 0)))
-                shape.append(Shape(5, 22, "J", size=(0, 0)))
+                shape.append(Shape(4, 20, "J", 1, size=(0, 0)))
+                shape.append(Shape(5, 20, "J", 1, size=(0, 0)))
+                shape.append(Shape(5, 21, "J", 1, size=(0, 0)))
+                shape.append(Shape(5, 22, "J", 1, size=(0, 0)))
             elif self.r == 5:
 
-                shape.append(Shape(4, 20, "Z", size=(0, 0)))
-                shape.append(Shape(5, 20, "Z", size=(0, 0)))
-                shape.append(Shape(5, 21, "Z", size=(0, 0)))
-                shape.append(Shape(6, 21, "Z", size=(0, 0)))
+                shape.append(Shape(4, 20, "Z", 1, size=(0, 0)))
+                shape.append(Shape(5, 20, "Z", 1, size=(0, 0)))
+                shape.append(Shape(5, 21, "Z", 1, size=(0, 0)))
+                shape.append(Shape(6, 21, "Z", 1, size=(0, 0)))
             elif self.r == 6:
 
-                shape.append(Shape(4, 20, "S", size=(0, 0)))
-                shape.append(Shape(4, 21, "S", size=(0, 0)))
-                shape.append(Shape(5, 21, "S", size=(0, 0)))
-                shape.append(Shape(5, 22, "S", size=(0, 0)))
+                shape.append(Shape(4, 20, "S", 1, size=(0, 0)))
+                shape.append(Shape(4, 21, "S", 1, size=(0, 0)))
+                shape.append(Shape(5, 21, "S", 1, size=(0, 0)))
+                shape.append(Shape(5, 22, "S", 1, size=(0, 0)))
             else:
 
-                shape.append(Shape(4, 20, "E", size=(0, 0)))
-                shape.append(Shape(4, 21, "E", size=(0, 0)))
-                shape.append(Shape(4, 22, "E", size=(0, 0)))
-                shape.append(Shape(5, 21, "E", size=(0, 0)))
+                shape.append(Shape(4, 20, "E", 1, size=(0, 0)))
+                shape.append(Shape(4, 21, "E", 1, size=(0, 0)))
+                shape.append(Shape(4, 22, "E", 1, size=(0, 0)))
+                shape.append(Shape(5, 21, "E", 1, size=(0, 0)))
 
         self.shapes.append(shape)
 
-        self.r = random.randint(1, 7)
+        self.r = 1#random.randint(1, 7)
 
     def collisions_bottom(self):
         for i in self.shapes[0:-1]:
@@ -205,13 +241,15 @@ class Shape(Rectangle):
     position_y = 0
     active = True
     brick = ""
+    version = 1
 
-    def __init__(self, position_x, position_y, brick, **kwargs):
+    def __init__(self, position_x, position_y, brick, version, **kwargs):
         super().__init__(**kwargs)
 
         self.position_x = position_x
         self.position_y = position_y
         self.brick = brick
+        self.version = version
 
 
 class App(BoxLayout):
@@ -234,6 +272,8 @@ class App(BoxLayout):
             self.tetris.move_left()
         if keycode[1] == "right":
             self.tetris.move_right()
+        if keycode[1] == "up":
+            self.tetris.rotate()
         if keycode[1] == "down":
             self.tetris.game_speed = 0.1
         return True
