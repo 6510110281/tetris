dc7f8455d9d89949b5d4dcb0fc1e468b52b9416b
Poramee Promkeeree <poramee0013@gmail.com>
2023-03-04 01:12:45 +0700

    เพิ่มรูปทรงใหม่และRandomshapecolors

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.graphics.vertex_instructions import Rectangle
+from kivy.graphics.context_instructions import Color
+import random
 
 
 class TetrisApp(App):
@@ -32,6 +34,7 @@ class TetrisWidget(GridLayout):
             obj = GridButton()
             self.add_widget(obj)
 
+        self.r = random.randint(1, 7)
         self.new_shape()
 
     def update_game(self, window_width, dt):
@@ -56,8 +59,64 @@ class TetrisWidget(GridLayout):
     def new_shape(self):
         shape = []
         with self.canvas:
-            shape.append(Shape(4, 20, size=(0, 0)))
+            colors = [
+                (1, 0, 0),    # red
+                (1, 0.5, 0),  # orange
+                (1, 1, 0),    # yellow
+                (0, 1, 0),    # green
+                (0, 1, 1),    # cyan
+                (0, 0, 1),    # blue
+                (0.5, 0, 1),  # purple
+            ]
+
+            color = random.choice(colors)
+            Color(*color)
+            if self.r == 1:
+
+                shape.append(Shape(4, 20, "I", size=(0, 0)))
+                shape.append(Shape(4, 21, "I", size=(0, 0)))
+                shape.append(Shape(4, 22, "I", size=(0, 0)))
+                shape.append(Shape(4, 23, "I", size=(0, 0)))
+            elif self.r == 2:
+
+                shape.append(Shape(4, 20, "O", size=(0, 0)))
+                shape.append(Shape(5, 20, "O", size=(0, 0)))
+                shape.append(Shape(4, 21, "O", size=(0, 0)))
+                shape.append(Shape(5, 21, "O", size=(0, 0)))
+            elif self.r == 3:
+
+                shape.append(Shape(4, 20, "L", size=(0, 0)))
+                shape.append(Shape(4, 21, "L", size=(0, 0)))
+                shape.append(Shape(4, 22, "L", size=(0, 0)))
+                shape.append(Shape(5, 20, "L", size=(0, 0)))
+            elif self.r == 4:
+
+                shape.append(Shape(4, 20, "J", size=(0, 0)))
+                shape.append(Shape(5, 20, "J", size=(0, 0)))
+                shape.append(Shape(5, 21, "J", size=(0, 0)))
+                shape.append(Shape(5, 22, "J", size=(0, 0)))
+            elif self.r == 5:
+
+                shape.append(Shape(4, 20, "Z", size=(0, 0)))
+                shape.append(Shape(5, 20, "Z", size=(0, 0)))
+                shape.append(Shape(5, 21, "Z", size=(0, 0)))
+                shape.append(Shape(6, 21, "Z", size=(0, 0)))
+            elif self.r == 6:
+
+                shape.append(Shape(4, 20, "S", size=(0, 0)))
+                shape.append(Shape(4, 21, "S", size=(0, 0)))
+                shape.append(Shape(5, 21, "S", size=(0, 0)))
+                shape.append(Shape(5, 22, "S", size=(0, 0)))
+            else:
+
+                shape.append(Shape(4, 20, "E", size=(0, 0)))
+                shape.append(Shape(4, 21, "E", size=(0, 0)))
+                shape.append(Shape(4, 22, "E", size=(0, 0)))
+                shape.append(Shape(5, 21, "E", size=(0, 0)))
+
         self.shapes.append(shape)
+
+        self.r = random.randint(1, 7)
 
     def collisions_bottom(self):
         for i in self.shapes[0:-1]:
@@ -110,11 +169,14 @@ class Shape(Rectangle):
     position_x = 0
     position_y = 0
     active = True
+    brick = ""
 
-    def __init__(self, position_x, position_y, **kwargs):
+    def __init__(self, position_x, position_y, brick, **kwargs):
         super().__init__(**kwargs)
+
         self.position_x = position_x
         self.position_y = position_y
+        self.brick = brick
 
 
 class App(BoxLayout):
