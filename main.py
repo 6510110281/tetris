fb31dc681bfa93891a5fb20c81f74d087329b2c8
Thanongsak Saengsak <aomsinsaengsak@gmail.com>
2023-03-04 00:04:29 +0700

    เพิ่มการตรวจสอบการชนของ Block(ขอบล่าง)

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ class TetrisWidget(GridLayout):
     def update_game(self, window_width, dt):
         self.fall(dt)
         self.size_change(window_width)
+        self.collisions_bottom()
 
     def fall(self, dt):
         self.time += dt
@@ -57,10 +58,26 @@ class TetrisWidget(GridLayout):
             shape.append(Shape(4, 20, size=(0, 0)))
         self.shapes.append(shape)
 
+    def collisions_bottom(self):
+        for i in self.shapes[0:-1]:
+            for j in i:
+                for k in self.shapes[-1]:
+                    if j.position_y + 1 == k.position_y and j.position_x == k.position_x:
+                        for j in self.shapes[-1]:
+                            j.active = False
+        for i in self.shapes[-1]:
+            if i.active == True:                        
+                if i.position_y == 0:
+                    for j in self.shapes[-1]:
+                        j.active = False                 
+            else:
+                self.new_shape()
+                return True             
 
 class Shape(Rectangle):
     position_x = 0
     position_y = 0
+    active = True
 
     def __init__(self, position_x, position_y, **kwargs):
         super().__init__(**kwargs)
